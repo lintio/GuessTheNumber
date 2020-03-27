c39a2596b79e6e08110e03c13d9f9c06517e3bc9
lintio <rmkayne@live.com>
2020-03-27 13:41:32 +0000

    changed the name of the readme file to ReadMe

--- guess.py
+++ guess.py
@@ -1,10 +1,10 @@
 import random, os
 
-answer = random.randint(1, 10)
+answer = random.randint(1, 20)
 guess = 100
 
 os.system('cls' if os.name == 'nt' else 'clear')
-print('Guess the number between 1 and 10 (type 0 to quit)')
+print('Guess the number between 1 and 20 (type 0 to quit)')
 while guess != 0:
     userInput = input('Your guess -> ')
     guess = int(userInput)
@@ -12,14 +12,14 @@ while guess != 0:
         break
     elif guess == answer:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print('Guess the number between 1 and 10 (type 0 to quit)')
+        print('Guess the number between 1 and 20 (type 0 to quit)')
         print('Well done!! you guessed the correct number (a new number has been selected)')
-        answer = random.randint(1, 10)
+        answer = random.randint(1, 20)
     elif guess > answer:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print('Guess the number between 1 and 10 (type 0 to quit)')
+        print('Guess the number between 1 and 20 (type 0 to quit)')
         print('Your guess was to high try again')
     elif guess < answer:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print('Guess the number between 1 and 10 (type 0 to quit)')
+        print('Guess the number between 1 and 20 (type 0 to quit)')
         print('your guess is to low try again')
